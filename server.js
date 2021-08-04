196d84f34a65333bc99621e052c1d95c2825fc3b
NayanSidhu <nsidh068@uottawa.ca>
2021-08-03 23:26:04 -0400

    user adding data

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+const fs = require('fs');
+const path = require('path');
 const {animals} = require('./data/animals.json'); //added data from the location into a variable 
 const express = require('express'); //Initiated express - have to thing  
 const PORT = process.env.PORT || 3001;
 const app = express(); 
+//parse incoming string data or array data 
+app.use(express.urlencoded({ extended: true}));
+//parse incoming JSON data
+app.use(express.json());
 
 //Filtering Data based off the query. 
 function filterByQuery(query, animalsArray){
@@ -43,6 +49,42 @@ function findById(id, animalsArray){
     return result; 
 }
 
+
+//This function will allow the POST data to be added from req.body to animals.json file
+function createNewAnimal(body, animalsArray){
+    const animal = body; 
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({animals: animalsArray}, null, 2)
+    );
+    console.log(body);
+    //function's main code goes over here
+    req.body.id = animals.length.toString(); //set ID based on what the next index of the array will be
+    //add animal to json file and animals array in this function 
+    const animal = createNewAnimal(req.body, animals);
+    //return finished code to post route for response
+    return animal; 
+}
+
+//This function validates the authenticity of the Animals
+function validateAnimal (animal){
+    if(!animal.name) || typeof animal.name !== 'string'){
+        return false; 
+    }
+    if(!animal.species) || typeof animal.species !== 'string'){
+        return false; 
+    }
+    if(!animal.diet) || typeof animal.diet !== 'string'){
+        return false; 
+    }
+    if(!animal.personalityTraits) || !Array.isArray(animal.personalityTraits)){
+        return false; 
+    }
+    return true; 
+}
+
+
 //Adding route using GET; required 2 arguments: 1) The route client will have to fetch data from, 2) The call back function on that route 
 app.get('/api/animals', (req, res) => {
     //Here all the animals data is loaded. 
@@ -65,7 +107,26 @@ app.get('/api/animals/:id', (req, res) => {
         res.send(404);
     }
     
-})
+});
+
+//Always have POST after GET method
+app.post('api/animals', (req,res) => {
+    //All animals have associated ID, but we can't ask the client to add an ID as they won't know if its already taken. So we auto gen IDs
+    req.body.id = animals.length.toString(); 
+    
+    //Checking to see if the data entered by user does infact validates 
+    if (!validateAnimal(req.body)){
+        res.status(400).send('The animal is not properly formatted.');
+    }else{
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
+
+    //req.body is where there incoming content that is going to the server will be 
+    console.log(req.body); //req.body property is here we can access that data on the server side and do something with it
+    res.json(req.body); //We are posting it to the server side and then sending it back to the client using response.json
+});
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
